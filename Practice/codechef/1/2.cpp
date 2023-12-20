8d97b3a0b09965e3db12942b44ec98ac49d13ff8
Megha Chauhan <megha.chauhan376@gmail.com>
2023-12-20 22:58:44 +0530

    codechef 20/12

--- Practice/codechef/1/2.cpp
+++ Practice/codechef/1/2.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool canMakeZero(int n) {
-    if (n == 0) {
-        return true;
-    }
-    if (n < 0) {
-        return false;
-    }
-    return canMakeZero(n - 3) || canMakeZero(n - 4);
-}
-
 int main() {
 	// your code goes here
 	int t;  cin >> t;
 	while(t--){
 	    int n;  cin >> n;
-	    if(canMakeZero(n)){
-	        cout << "YES" << "\n";
+	    if(n<=2 || n==5){
+	        cout << "NO" << "\n";
 	    }
 	    else{
-	        cout << "NO" << "\n";
+	        cout << "YES" << "\n";
 	    }
 	}
 	return 0;
